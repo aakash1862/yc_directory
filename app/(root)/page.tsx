8be35127aea598d14a5c90a04d3b9cc44fc70ac4
Aakash Dixit <aakash.dixit@natwest.com>
2025-06-06 23:04:12 +0530

    Commit till Create Form(4:05:57)

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -13,7 +13,7 @@ export default async function Home({ searchParams }: {
   const query = (await searchParams).query;
   const params = { search: query || null };
   const session = await auth();
-  console.log(session?.id);
+  // console.log(session?.id);
   const { data: posts } = await sanityFetch( { query: STARTUP_QUERY, params } );
 
   return (
